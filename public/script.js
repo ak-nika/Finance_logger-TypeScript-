40a584e7c03ac16cf56904c30543c2155e89dafd
ak-nika <akingbayiojo@gmail.com>
2024-05-31 13:12:17 +0100

    Function types

--- public/script.js
+++ public/script.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('test');
+let greet;
+// greet = 'hello';
+greet = () => {
+    console.log('hello');
+};
+const add = (a, b, c = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, '20');
+const minus = (a, b) => {
+    return a - b;
+};
+let result = minus(10, 7); //This sets the result to the return value as well as the type
