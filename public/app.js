307efee7c981a4ea53b0b60fbfe124b680605989
ak-nika <akingbayiojo@gmail.com>
2024-06-03 14:14:16 +0100

    Lesson 15: interfaces

--- public/app.js
+++ public/app.js
@@ -1,3 +1,20 @@
+const me = {
+    name: 'Shaun',
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log("Hello ", person.name);
+};
+greetPerson(me);
+let someone;
+console.log(me);
 import { Invoice } from './classes/invoice';
 const inOne = new Invoice('mario', 'work on the mario website', 250);
 const inTwo = new Invoice('luigi', 'work on the luigi website', 300);
@@ -10,7 +27,7 @@ invoices.forEach(inv => {
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
-const toFrom = document.querySelector("#tofrom");
+const toFrom = document.querySelector("#toFrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener("submit", (e) => {
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,33 @@
+// interfaces
+interface isPerson {
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: isPerson = {
+    name: 'Shaun',
+    age: 30,
+    speak(text: string): void {
+        console.log(text);
+    },
+    spend(amount: number): number{
+        console.log('I spent',amount);
+        return amount;
+    }
+};
+
+const greetPerson = ( person: isPerson ) => {
+    console.log("Hello ", person.name);
+}
+
+greetPerson(me)
+
+let someone: isPerson;
+
+console.log(me)
+
 import { Invoice } from './classes/invoice';
 
 const inOne = new Invoice('mario', 'work on the mario website', 250);
@@ -8,14 +38,14 @@ invoices.push(inOne)
 invoices.push(inTwo)
 
 invoices.forEach(inv => {
-    console.log(inv.client, inv.amount, inv.format())
+    console.log(inv.client, inv.amount, inv.format());
 })
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 // inputs
 const type = document.querySelector("#type") as HTMLSelectElement;
-const toFrom = document.querySelector("#tofrom") as HTMLInputElement;
+const toFrom = document.querySelector("#toFrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
